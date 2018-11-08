d57c2263f1c851b609445329ef095ebc6be2e6d9
lynzt <techells22@gmail.com>
2018-11-08 14:44:48 -0600

    init commit

---
+++ elpa/clojure-mode-extra-font-locking-20180114.1711/clojure-mode-extra-font-locking-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "clojure-mode-extra-font-locking" "20180114.1711" "Extra font-locking for Clojure mode" '((clojure-mode "3.0")) :commit "71b32ee95e8ec99098e212cd0ec74cb4c002e8c6" :keywords '("languages" "lisp") :authors '(("Bozhidar Batsov" . "bozhidar@batsov.com")) :maintainer '("Bozhidar Batsov" . "bozhidar@batsov.com") :url "http://github.com/clojure-emacs/clojure-mode")
---
+++ init.el
@@ -0,0 +1,43 @@
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(unless (package-installed-p 'spacemacs-theme)
+  (package-refresh-contents)
+  (package-install 'spacemacs-theme))
+
+(use-package which-key
+  :ensure t
+  :init
+  (which-key-mode))
+
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+(setq inhibit-startup-screen t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (spacemacs-dark)))
+ '(custom-safe-themes
+   (quote
+    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
+ '(package-selected-packages
+   (quote
+    (spacemacs-theme zenburn-theme which-key use-package try projectile org nyan-mode neotree monokai-theme monokai-alt-theme molokai-theme labburn-theme hc-zenburn-theme company clojure-mode-extra-font-locking cider anti-zenburn-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Droid Sans Mono Dotted for Powerline")))))
