a854fcc2148c96a20c2f5a173e32c91f65f1763d
lynzt <techells22@gmail.com>
2018-11-11 20:01:19 -0600

    show invisibles

--- config.el
+++ config.el
@@ -228,3 +228,12 @@
     ("C-c b" . ztree-diff))
   :init
   (setq ztree-dir-move-focus t))
+
+(global-whitespace-mode t)
+(setq whitespace-display-mappings
+  ;; all numbers are Unicode codepoint in decimal. ⁖ (insert-char 182 1)
+  '(
+    (space-mark 32 [183] [46]) ; 32 SPACE 「 」, 183 MIDDLE DOT 「·」, 46 FULL STOP 「.」
+    (newline-mark 10 [8629 10]) ; 10 LINE FEED
+    (tab-mark 9 [9655 9] [92 9]) ; 9 TAB, 9655 WHITE RIGHT-POINTING TRIANGLE 「▷」
+    ))
